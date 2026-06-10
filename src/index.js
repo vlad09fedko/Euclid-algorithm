00859c1badb763e9c758fa9cc7c74210b0579b91
foramer <vlad09fedko@gmail.com>
2026-06-10 21:02:02 +0000

    Do app

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-'use strict'
+'use strict';
+
+
+function findGreatestCommonDivisor(a, b) {
+  if (b === 0) return a;
+  return findGreatestCommonDivisor(b, a % b);
+}
+
+const start = Date.now();
+console.log(findGreatestCommonDivisor(1239, 301));
+console.log(`Lead time: ${Date.now() - start} ms`);
