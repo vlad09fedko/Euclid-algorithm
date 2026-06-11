b3b5f12e2203f91d2ff2ccb41f2b8dd0b2026c44
foramer <vlad09fedko@gmail.com>
2026-06-11 23:36:15 +0000

    Add error handling

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 'use strict';
 
-
 function findGreatestCommonDivisor(a, b) {
+  if (!Number.isFinite(a || b)) throw TypeError('Data must be numbers!');
   if (b === 0) return a;
   return findGreatestCommonDivisor(b, a % b);
 }
 
-const start = Date.now();
-console.log(findGreatestCommonDivisor(1239, 301));
-console.log(`Lead time: ${Date.now() - start} ms`);
+try {
+  const start = Date.now();
+  console.log(findGreatestCommonDivisor(1239, 301));
+  console.log(`Lead time: ${Date.now() - start} ms`);
+} catch (error) {
+  console.error(error.message);
+}
